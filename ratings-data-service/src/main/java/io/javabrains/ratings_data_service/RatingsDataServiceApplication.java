81965974fc0eb442f910c78c1438630fd1136e69
Libin <Libin@LAPTOP-0U7218MK>
2025-01-08 14:04:52 +0530

    RatingsDataService Commit.

---
+++ ratings-data-service/src/main/java/io/javabrains/ratings_data_service/RatingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.ratings_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingsDataServiceApplication.class, args);
+	}
+
+}
---
+++ ratings-data-service/src/main/java/io/javabrains/ratings_data_service/resources/RatingsResource.java
@@ -0,0 +1,30 @@
+package io.javabrains.ratings_data_service.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.ratings_data_service.models.Rating;
+import io.javabrains.ratings_data_service.models.UserRating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+
+}
